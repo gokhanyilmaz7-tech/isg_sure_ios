2224a560e66b7c2083480fba1f31a0821c9789df
Gökhan Yılmaz <gokhanyilmaz7@icloud.com>
2026-03-18 22:55:52 +0300

    Initial Commit

---
+++ isg_sure_os/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  isg_sure_os
+//
+//  Created by GökhanMacbook on 18.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ isg_sure_os/isg_sure_osApp.swift
@@ -0,0 +1,17 @@
+//
+//  isg_sure_osApp.swift
+//  isg_sure_os
+//
+//  Created by GökhanMacbook on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct isg_sure_osApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
